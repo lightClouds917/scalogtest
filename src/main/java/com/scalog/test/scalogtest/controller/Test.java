92bcaf9782f1893b4a72bbf1bb5b1b20b746b553
汪忠祥 <1186355422@qq.com>
2020-07-30 20:45:54 +0800

    optimize:the dmoe

--- src/main/java/com/scalog/test/scalogtest/controller/Test.java
+++ src/main/java/com/scalog/test/scalogtest/controller/Test.java
@@ -1,6 +1,7 @@
 package com.scalog.test.scalogtest.controller;
 
 import com.java4all.scalog.annotation.LogInfo;
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.ServiceLoader;
 import javax.sql.DataSource;
@@ -21,30 +22,20 @@ public class Test {
     @Autowired
     private Environment environment;
 
+    /**
+     * 测试url:http://localhost:1111/test/test?name=aaaaaaaa
+     * @param name
+     * @return
+     * @throws Exception
+     */
     @LogInfo(moduleName = "金融模块",functionName = "扣减授信",remark = "测试用的")
     @GetMapping("test")
     public String test(String name) throws Exception {
-        System.out.println("lalalal"+name);
-        Map<String, String> getenv = System.getenv();
+        System.out.println("执行成功:"+ LocalDateTime.now().toString()+":" +name);
         System.out.println(environment.getProperty("scalog.countryName"));
-
-        return name;
+        return "执行成功:"+ LocalDateTime.now().toString()+":" +name;
     }
 
-//    private DataSource get() throws Exception {
-//        ServiceLoader<IDataSource> load = ServiceLoader.load(IDataSource.class);
-//        System.out.println(load.iterator().hasNext());
-//        for(IDataSource dataSource : load){
-//            System.out.println(dataSource.getClass().getName());
-//            IDataSource dataSource1 = dataSource.getClass().newInstance();
-//            Object dataSource2 = dataSource1.getDataSource();
-//            System.out.println(dataSource2);
-//            Class<?> aClass = Class.forName(dataSource.getClass().getName(), true,
-//                    this.getClass().getClassLoader());
-//        }
-//
-//        return null;
-//    }
 }
-//https://www.cnblogs.com/yjbjingcha/p/6752265.html
-//        https://blog.csdn.net/u010502101/article/details/78705030
+
+
