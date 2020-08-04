41efab7c48d530ec04923e2989ab36d49c14d721
汪忠祥 <1186355422@qq.com>
2020-08-04 14:30:22 +0800

    add:添加postgresql测试

--- src/main/java/com/scalog/test/scalogtest/ScalogtestApplication.java
+++ src/main/java/com/scalog/test/scalogtest/ScalogtestApplication.java
@@ -1,14 +1,11 @@
 package com.scalog.test.scalogtest;
 
-import com.java4all.scalog.properties.springcloud.EnableScalogSpringConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @ComponentScan({"com.*","com.java4all"})
 @SpringBootApplication
-@EnableScalogSpringConfig
 public class ScalogtestApplication {
 
     public static void main(String[] args) {
